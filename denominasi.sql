f728b67518a5cfb70fc5271005a8984505743ebe
Pieter Haro <pieter.haro@gmail.com>
2020-04-07 19:57:58 +0700

    Add files via upload

---
+++ denominasi.sql
@@ -0,0 +1,101 @@
+/*
+SQLyog Community v13.1.5  (64 bit)
+MySQL - 10.4.8-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `denominasi` (
+	`id_denominasi` int (11),
+	`nama_lengkap_denominasi` varchar (300),
+	`nama_pendek_denominasi` varchar (30)
+); 
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('1','Huria Kristen Batak Protestan','HKBP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('2','Banua Niha Keriso Protestan','BNKP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('3','Gereja Batak Karo Protestan','GBKP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('4','Gereja Methodist Indonesia','GMI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('5','Gereja Kalimantan Evangelis','GKE');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('6','Gereja Masehi Injili Sangihe Talaud','GMIST');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('7','Gereja Masehi Injili Minahasa','GMIM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('8','Gereja Masehi Injili Di Bolaang Mongondow','GMIBM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('9','Gereja Kristen Sulawesi Tengah','GKST');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('10','Gereja Toraja','GETOR');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('11','Gereja Toraja Mamasa','GTM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('12','Gereja Kristen di Sulawesi Selatan','GKSS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('13','Gereja Protestan di Sulawesi Tenggara','GEPSULTRA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('14','Gereja Masehi Injili Halmahera','GMIH');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('15','Gereja Protestan Maluku','GPM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('16','Gereja Kristen Injili di Tanah Papua','GKITP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('17','Gereja Masehi Injili di Timor','GMIT');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('18','Gereja Kristen Sumba','GKS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('19','Gereja Kristen Protestan di Bali','GKPB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('20','Gereja Kristen Jawi Wetan','GKJW');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('21','Gereja Kristen Indonesia','GKI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('22','Gereja Injili di Tanah Jawa','GITJ');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('23','Gereja Kristen Jawa','GKJ');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('24','Gereja Kristen Pasundan','GKP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('25','Gereja Kristus','GK');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('26','Gereja Protestan di Indonesia bagian Barat','GPIB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('27','Gereja Protestan di Indonesia','GPI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('28','Gereja Isa Almasih','GIA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('29','Gereja Kristen Muria Indonesia','GKMI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('30','Gereja Kristen Protestan Simalungun','GKPS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('31','Gereja Kristen Pemancar Injil','GKPI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('32','Gereja Bethel Injil Sepenuh','GBIS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('33','Gereja Pantekosta Pusat Surabaya','GPPS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('34','Huria Kristen Indonesia','HKI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('35','Gereja Kristen di Luwuk Banggai','GKLB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('36','Gereja Kristus Tuhan','GKT');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('37','Gereja Protestan Indonesia Donggala','GPID');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('38','Gereja Punguan Kristen Batak','GPKB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('39','Gereja Protestan Indonesia Gorontalo','GPIG');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('40','Gereja Kristen Jawa Tengah Utara','GKJTU');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('41','Gereja Kristen Kalimantan Barat','GKKB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('42','Gereja Gerakan Pantekosta','GGP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('43','Gereja Kristen Protestan Indonesia','GKPI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('44','Gereja Protestan Indonesia di Buol Toli-toli','GPIBT');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('45','Gereja Kristen Protestan Mentawai','GKPM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('46','Gereja Kristen di Indonesia di Sumatera Utara','GKI SUMUT');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('47','Gereja Kristen Protestan Angkola','GKPA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('48','Kerapatan Gereja Protestan Minahasa','KGPM');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('49','Gereja Mission Batak','GMB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('50','Gereja Angowuloa Masehi Indonesia Nias','Gereja AMI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('51','Gereja Kristen Anugerah','GKA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('52','Gereja Protestan Indonesia Luwu','GPIL');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('53','Gereja Kebangunan Kalam Allah','GKKA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('54','Gereja Kristen Kalam Kudus','GKKK');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('55','Orahua Niha Keriso Protestan','ONKP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('56','Gereja Kristen Sumatera Bagian Selatan','GKSBS');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('57','Gereja Protestan Kalimantan Barat','GPKB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('58','Gereja Bethel Indonesia','GBI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('59','Gereja Kristen Injili Indonesia','GKII');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('60','Gereja Masehi Injili Indonesia','GEMINDO');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('61','Gereja Kristen Injili di Indonesia','GEKISIA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('62','Gereja Kristen Luther Indonesia','GKLI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('63','Gereja Protestan Persekutuan','GPP');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('64','Gereja Kristen Setia Indonesia','GKSI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('65','Gereja Tuhan di Indonesia','GTDI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('66','Gereja Kristen Indonesia di Sulawesi Selatan','GKI SULSEL');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('67','Gereja Kristen Perjanjian Baru','GKPB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('68','Angowuloa Fa’awosa Kho Yesu','AFY');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('69','Gereja Rehoboth','GR');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('70','Gereja Protestan Indonesia di Papua','GPI PAPUA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('71','Gereja Kristen Protestan Pakpak Dairi','GKPPD');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('72','Gereja Keesaan Injili Indonesia','GEKINDO');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('73','Gereja Masehi Protestan Umum','GMPU');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('74','Gereja Kristen Sulawesi Barat','GKSB');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('75','Gereja Kristen Oikoumene di Indonesia','GKO');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('76','Gereja Sahabat Indonesia','GSI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('77','Gereja Utusan Pantekosta di Indonesia','GUPDI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('78','Gereja Protestan Indonesia di Banggai Kepulauan','GPIBK');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('79','Gereja Masehi Injili di Talaud','GERMITA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('80','Gereja Kristen Abdiel','GKA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('81','Gereja Kristus Rahmani Indonesia','GKRI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('82','Gereja Sidang-sidang Jemaat Allah','GSJA');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('83','Gereja Kristus Yesus','GKY');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('84','Gereja Kristen Protestan Injili Indonesia','GKPII');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('85','Gereja Kemah Injil Indonesia','GKII');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('86','Gereja Protestan Soteria di Indonesia','GPSI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('87','Gereja Kristen Sangkakala Indonesia','GKSI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('88','Kerukunan Gereja Masehi Protestan Indonesia','KGMPI');
+insert into `denominasi` (`id_denominasi`, `nama_lengkap_denominasi`, `nama_pendek_denominasi`) values('89','Jemaat Kristen Indonesia','JKI');
